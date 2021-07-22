2895433b560c5cf2b5c056aa3f5122783e81db3e
sxiao730 <sxiao730@gmail.com>
2021-07-22 17:48:58 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data analysis models and tools for Physician Compensation',
+    author='sxiao730',
+    license='MIT',
+)
